95d004b27c41aa75852b35b51ba44e39b703ae6d
unknown <bojanm245@gmail.com>
2022-07-05 13:00:29 +0200

    Added a check that prevents a user from deleting their own account

--- PizzaVilla.Implementation/Validators/Users/DeleteUserValidator.cs
+++ PizzaVilla.Implementation/Validators/Users/DeleteUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using PizzaVilla.DataAccess;
+using PizzaVilla.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,13 @@ namespace PizzaVilla.Implementation.Validators.Users
 {
     public class DeleteUserValidator : AbstractValidator<int>
     {
-        public DeleteUserValidator(PVDbContext context)
+        public DeleteUserValidator(PVDbContext context, IApplicationActor user)
         {
             RuleFor(x => x)
                 .Must(x => context.Users.Any(y => y.Id == x))
-                .WithMessage("A user with the id of {PropertyValue} doesn't exist.");
+                .WithMessage("A user with the id of {PropertyValue} doesn't exist.")
+                .Must(x => x != user.Id)
+                .WithMessage("You can't delete your own account.");
 
             // No other validation because the user is being soft deleted
         }
